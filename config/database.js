a07073c376961e19719871c7ea4ed3843a42351f
naoufalhatim <naoufal.hatim@usmba.ac.ma>
2016-04-09 04:20:23 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'your-database-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://appharbor_mw71gc76:pmoft40h2rc4b9ntgk41ubpmgo@ds013260.mlab.com:13260/appharbor_mw71gc76'
+	//  or looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
