4ae8ec793840d4cb68bc93979441e4a6e6e2901c
naoufalhatim <naoufal.hatim@usmba.ac.ma>
2016-04-10 04:38:11 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://appharbor_mw71gc76:pmoft40h2rc4b9ntgk41ubpmgo@ds013260.mlab.com:13260/appharbor_mw71gc76'
+	//'url' : 'mongodb://appharbor_mw71gc76:pmoft40h2rc4b9ntgk41ubpmgo@ds013260.mlab.com:13260/appharbor_mw71gc76'
+	'url' : 'mongodb://naoufal:naoufal@ds019970.mlab.com:19970/naoufal1'
 	//  or looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
 };
